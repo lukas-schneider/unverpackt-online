832124eefb888045ef04ca431430e98c367a3706
Lukas Schneider <lukas.schneider.ac@gmail.com>
2024-11-16 11:03:55 +0100

    feat: svelte + vite

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import postcss from './postcss.config.js';
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [svelte()],
+  css:{
+    postcss
+  }
+})
